054ef566dc5cd5391aaa220d34b46557110b72bf
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-13 15:45:00 +0530

    Create M13a.c

---
+++ M13a.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,f,g,h,i;
+clrscr();
+printf("Enter the Ninja's in Kabali's Clan in Two Range:");
+scanf("%d%d",&a,&b);
+c=a-b;
+printf("Difference :%d",c);
+printf("\nOponent Team Ninja's:");
+scanf("%d%d",&d,&e);
+f=d-e;
+printf("\nDifferece:%d",f);
+printf("\nEnter the Range:");
+scanf("%d%d",&g,&h);
+i=g-h;
+printf("\nDifference:%d",i);
+getch();
+}
