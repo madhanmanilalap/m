8c214fa5dc3b805ff1bb9865d6330ad1ff2891be
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-13 16:00:39 +0530

    Create M13h.c

---
+++ M13h.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,i,c,d;
+clrscr();
+printf("Enter the Value a:");
+scanf("%d",&a);
+printf("Enter the value b:");
+scanf("%d",&b);
+for(i=1;i<=a && i<=b;i++)
+{
+if(a%i==0 && b%i==0)
+{
+c=i;
+}}
+d=(a*b)/c;
+printf("L.C.M:%d",d);
+
+getch();
+}
