213061a8c9f3622e3b887b54a819656fcd496120
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-13 16:12:52 +0530

    Create M13b.c

---
+++ M13b.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d,e,f,g,h,i,j,k,l;
+clrscr();
+printf("Enter the values:");
+scanf("%d%d",&a,&b);
+c=a/b;
+printf("%d",c);
+scanf("%d%d",&e,&f);
+d=e%f;
+printf("%d",d);
+scanf("%d%d",&g,&h);
+i=g/h;
+printf("%d",i);
+scanf("%d%d",&j,&k);
+l=j%k;
+printf("%d",l);
+getch();
+}
