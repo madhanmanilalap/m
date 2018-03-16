082d23c65caad7a4d34db6e5f615c88c88d8e11f
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-16 15:00:13 +0530

    Create M11b.c

---
+++ M11b.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.c>
+void main()
+{
+int a,b,c;
+printf("Enter the Number:");
+scanf("%d",&a);
+printf("Enter the Power:");
+scanf("%d",&b);
+c=pow(b,a);
+printf("Answer:%d",c);
+}
