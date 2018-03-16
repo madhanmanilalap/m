bb32167b156586931adaf6af1b371f4e75406dff
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-16 15:03:46 +0530

    Create M11g.c

---
+++ M11g.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+clrscr();
+printf("Enter the Numbers:");
+scanf("%d%d%d",&a,&b,&c);
+d=(a*b)/c;
+printf("Answer:%d",d);
+getch();
+}
