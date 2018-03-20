1cddceae14cc20fa8d9a8a856887a4d17c046aed
madhanmanilalap <35652995+madhanmanilalap@users.noreply.github.com>
2018-03-20 16:42:28 +0530

    Create M12b.c

---
+++ M12b.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c[20],id=0;
+clrscr();
+printf("Enter the size:");
+scanf("%d",&a);
+printf("Enter the Numbers:\n");
+for(i=0;i<a;i++)
+{
+scanf("%d",&c[i]);
+}
+printf("Enter the Number:");
+scanf("%d",&b);
+for(i=0;i<a;i++)
+{
+if(a[i]==b)
+{
+d=1;
+}}
+if(d==1)
+printf("Yes");
+else
+printf("No");
+getch();
+}
